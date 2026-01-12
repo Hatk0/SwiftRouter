c1992f481218b3fde4f3cf50c65b0ba8ea013626
Dmitry Yastrebov <stewielime2018@gmail.com>
2026-01-12 18:58:24 +0500

    Base setup

--- Package.swift
+++ Package.swift
@@ -1,22 +1,26 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftRouter",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .tvOS(.v16),
+        .watchOS(.v9),
+        .visionOS(.v1)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftRouter",
             targets: ["SwiftRouter"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftRouter"
+            name: "SwiftRouter",
+            dependencies: []
         ),
         .testTarget(
             name: "SwiftRouterTests",
